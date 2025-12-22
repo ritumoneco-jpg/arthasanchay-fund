a447664fb71f14393958da47079d910b0fef09a3
Your Name <your-email@example.com>
2025-12-22 21:04:38 +0530

    Mobile responsiveness fix

--- app.py
+++ app.py
@@ -75,10 +75,10 @@ Message:
     return render_template("contact.html")
 
 # ---------- RUN APP ----------
+
+import os
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    
-    
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
     
